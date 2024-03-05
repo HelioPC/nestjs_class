cb38b0bd895ecd647484a348bd33b9fb514c59c7
Eliúde Vemba <sweeteliude@gmail.com>
2024-03-05 02:06:07 +0100

    Initial commit (Nest JS tutorial 🚀)

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  login() {
+    return 'I am signin';
+  }
+
+  signup() {
+    return 'I am signup';
+  }
+}
