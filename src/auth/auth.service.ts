2e58acc9a35f663850e771265eb95e0a4c0fba0d
Eliúde Vemba <sweeteliude@gmail.com>
2024-03-07 13:01:02 +0100

    Add sign up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -9,7 +12,27 @@ export class AuthService {
     return 'I am signin';
   }
 
-  signup() {
-    return 'I am signup';
+  async signup(dto: AuthDTO) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      delete user.password;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Invalid credentials');
+        }
+      }
+    }
   }
 }
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDTO {
+  @IsString()
+  firstName: string;
+
+  lastName: null | string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
