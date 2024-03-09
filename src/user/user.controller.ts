ea03496da2786ca8e160c7ed0fe473fa31b32884
Eliúde Vemba <sweeteliude@gmail.com>
2024-03-09 04:17:05 +0100

    Add user info endpoint

---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('user')
+export class UserController {
+  @Get('me')
+  get(@GetUser() user: User) {
+    console.log({
+      user: user,
+    });
+
+    return { data: user };
+  }
+}
