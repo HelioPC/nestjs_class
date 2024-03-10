33364d5bdf01528a8349549d78d62ee420fb4183
Eliúde Vemba <sweeteliude@gmail.com>
2024-03-10 01:11:29 +0100

    Test auth endpoints

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,11 @@
+import * as pactum from 'pactum';
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
+import { Faker, pt_PT } from '@faker-js/faker';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDTO, LoginDTO } from '../src/auth/dto';
+
 describe('App e2e', () => {
   let app: INestApplication;
   let prisma: PrismaService;
@@ -14,10 +18,13 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3000);
 
     prisma = app.get(PrismaService);
 
     await prisma.cleanDB();
+
+    pactum.request.setBaseUrl('http://localhost:3000');
   });
 
   afterAll(() => {
@@ -25,16 +32,81 @@ describe('App e2e', () => {
   });
 
   describe('Auth', () => {
+    const faker = new Faker({ locale: [pt_PT] });
+    const dto: AuthDTO = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
     describe('Sign up', () => {
-      it.todo('Should sign up');
+      const endpoint = '/auth/signup';
+
+      it('Should throw error if email empty', () => {
+        const dtoCopy = { ...dto };
+
+        delete dtoCopy.email;
+
+        return pactum.spec().post(endpoint).withBody(dtoCopy).expectStatus(400);
+      });
+
+      it('Should throw error if password empty', () => {
+        const dtoCopy = { ...dto };
+
+        delete dtoCopy.password;
+
+        return pactum.spec().post(endpoint).withBody(dtoCopy).expectStatus(400);
+      });
+
+      it('Should throw error if body empty', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+
+      it('Should register a user', () => {
+        return pactum.spec().post(endpoint).withBody(dto).expectStatus(201);
+      });
     });
 
     describe('Sign in', () => {
-      it.todo('Should sign in');
+      const endpoint = '/auth/signin';
+      const loginDTO: LoginDTO = {
+        email: dto.email,
+        password: dto.password,
+      };
+
+      it('Should throw error if email empty', () => {
+        const dtoCopy = { ...loginDTO };
+
+        delete dtoCopy.email;
+
+        return pactum.spec().post(endpoint).withBody(dtoCopy).expectStatus(400);
+      });
+
+      it('Should throw error if password empty', () => {
+        const dtoCopy = { ...loginDTO };
+
+        delete dtoCopy.password;
+
+        return pactum.spec().post(endpoint).withBody(dtoCopy).expectStatus(400);
+      });
+
+      it('Should throw error if body empty', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+
+      it('Should login a user', () => {
+        return pactum
+          .spec()
+          .post(endpoint)
+          .withBody(loginDTO)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      });
     });
   });
 
-  describe('User', () => {
+  /*describe('User', () => {
     describe('Get me', () => {
       it.todo('Should get user info');
     });
@@ -68,5 +140,5 @@ describe('App e2e', () => {
     describe('Delete a bookmark', () => {
       it.todo('Should delete a bookmark');
     });
-  });
+  });*/
 });
